2360dec00db99a771cfc3b99685a8eacf612d8c3
OhadR <ohadr.developer@gmail.com>
2022-01-31 18:59:27 +0200

    add async step

--- steps/stepdefs.js
+++ steps/stepdefs.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const { Given, When, Then } = require('@cucumber/cucumber');
+const fs = require('fs');
 
 function isItFriday(today) {
     return 'Nope';
@@ -15,4 +16,18 @@ When('I ask whether it\'s Friday yet', function () {
 
 Then('I should be told {string}', function (expectedAnswer) {
     assert.strictEqual(this.actualAnswer, expectedAnswer);
+});
+
+// Asynchronous - callback
+//
+// Take a callback as an additional argument to execute when the step is done
+Then(/^the file named (.*) is empty$/, function (fileName, callback) {
+    fs.readFile(fileName, 'utf8', function(error, contents) {
+        if (error) {
+            callback(error);
+        } else {
+            assert.equal(contents, '');
+            callback();
+        }
+    });
 });
